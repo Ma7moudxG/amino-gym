1cb049e38a2f1fd8d7a4efea25fa605d89a43f23
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-02-14 14:26:54 +0400

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,26 @@ import Button from './Button'
 
 export default function Hero() {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center 
-    justify-center max-w-[1000px] w-full mx-auto p-6">
-        <div className="flex flex-col gap-4 items-center">
-            <p className="text-xl">IT'S TIME TO GET TO</p>
-            <h1 className="uppercase font-semibold text-4xl 
-            sm:text-5xl md:text-6xl lg:text-7xl">Amino 
-            <span className="text-yellow-400">GYM</span>
-            </h1>
-        </div>
-        <p className="text-lg md:text-2xl font-light text-center ">
-            I hearby acknowledge that i may become <spn className="text-yellow-400 font-medium">unbelievably strong </spn>
-            and accept all risks of becoming the local <spn className="text-yellow-400 font-medium">mass montrosity</spn>, 
-            afflicted with severe body dismorphia, unable to fit though doors..
-        </p>
+    <div className="min-h-screen  bg-cover p-6 shadow-lg bg-blend-overlay  dark:bg-neutral-800 bg-hero">
+        <div className='flex flex-col gap-10 items-center 
+    justify-center w-full  mx-auto min-h-screen max-w-[1000px]'>
+          <div className="flex flex-col gap-4 items-center">
+              <p className="text-xl">IT'S TIME TO GET TO</p>
+              <h1 className="uppercase font-semibold text-4xl 
+              sm:text-5xl md:text-6xl lg:text-7xl">Amino 
+              <span className="text-yellow-400">GYM</span>
+              </h1>
+          </div>
+          <p className="text-lg md:text-2xl font-light text-center ">
+              I hearby acknowledge that i may become <spn className="text-yellow-400 font-medium">unbelievably strong </spn>
+              and accept all risks of becoming the local <spn className="text-yellow-400 font-medium">mass montrosity</spn>, 
+              afflicted with severe body dismorphia, unable to fit though doors..
+          </p>
 
-        <Button func={() => 
-            window.location.href = '#generate'
-        } text={"Accept & Begin"}/>
+          <Button func={() => 
+              window.location.href = '#generate'
+          } text={"Accept & Begin"}/>
+        </div>
     </div>
   )
 }
